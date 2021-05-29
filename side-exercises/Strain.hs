79c819fb57156e129e00cbacc0ed4850e1c97178
meghaae <meghaae@outlook.in>
2021-05-29 21:57:29 +0530

    Add solution to 2nd side exercise

---
+++ side-exercises/Strain.hs
@@ -0,0 +1,22 @@
+module Strain (keep, discard) where
+
+
+keep :: (a -> Bool) -> [a] -> [a]
+keep p xs = [x | x <- xs, p x] 
+
+discard :: (a -> Bool) -> [a] -> [a]
+discard p xs = keep (not . p) xs
+
+
+{-
+	SOLUTION USING RECURSION:
+
+	keep :: (a -> Bool) -> [a] -> [a]
+	keep _ [] = []
+	keep p (x:xs) | p x = x : keep p xs
+	              | otherwise = keep p xs 
+
+
+	discard :: (a -> Bool) -> [a] -> [a]
+	discard p xs = keep (not . p) xs
+-}
