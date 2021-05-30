d5efa2e95806a6b3b3d761588c0a98f9249a6e0a
meghaae <meghaae@outlook.in>
2021-05-30 16:41:41 +0530

    Add /'Instructions/' to 2nd core exercise file

--- core-exercises/LeapYear.hs
+++ core-exercises/LeapYear.hs
@@ -16,21 +16,23 @@
 -}
 
 
+
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year = (rem year 4 == 0) && (rem year 100 /= 0 || rem year 400 == 0) 
 
 
-{-  Following is for my own understanding: 
 
-	2015		F && ...			= F
-	1970		F && ...			= F
-	1996		T && (T||...)			= T
-	1960		T && (T||...) 			= T
-	2100		T && (F||F)			= F
-	1900		T && (F||F)			= F
-	2000		T && (T||...)			= T
-	2400		T && (T||...)			= T
-	1800		T && (F||F)			= F
+{-  for my own understanding-- 
+
+	2015	F && ...		= F
+	1970	F && ...		= F
+	1996	T && (T||...)	= T
+	1960	T && (T||...) 	= T
+	2100	T && (F||F)		= F
+	1900	T && (F||F)		= F
+	2000	T && (T||...)	= T
+	2400	T && (T||...)	= T
+	1800	T && (F||F)		= F
 -} 
