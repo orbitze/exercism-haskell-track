e294869bcd8d54a563a2464b8692fb6d0c1453d5
meghaae <meghaae@outlook.in>
2021-05-30 16:53:24 +0530

    Add 'Instructions' to 3rd side-exercise file

--- side-exercises/Phone.hs
+++ side-exercises/Phone.hs
@@ -1,3 +1,41 @@
+
+{-                                INSTRUCTIONS
+
+    Clean up user-entered phone numbers so that they can be sent SMS messages.
+
+    The North American Numbering Plan (NANP) is a telephone numbering system used by many countries 
+    in North America like the United States, Canada or Bermuda. All NANP-countries share the same 
+    international country code: 1.
+
+    NANP numbers are ten-digit numbers consisting of a three-digit Numbering Plan Area code, commonly known
+    as area code, followed by a seven-digit local number. The first three digits of the local number
+    represent the exchange code, followed by the unique four-digit number which is the subscriber number.
+
+    The format is usually represented as
+
+    (NXX)-NXX-XXXX
+    where N is any digit from 2 through 9 and X is any digit from 0 through 9.
+
+    Your task is to clean up differently formatted telephone numbers by removing punctuation 
+    and the country code (1) if present.
+
+    For example, the inputs
+
+    +1 (613)-995-0253
+    613-995-0253
+    1 613 995 0253
+    613.995.0253
+    should all produce the output
+
+    6139950253
+
+    Note: As this exercise only deals with telephone numbers used in NANP-countries, 
+    only 1 is considered a valid country code.
+-}
+
+
+
+
 module Phone (number) where
 
 
@@ -8,39 +46,26 @@ number xs | isValid   = Just plainStr
 
            where plainStr = makePlain
 
+                 --takes the given string and returns another string consisting of only
+                 --only such chars that represent numbers, disregards everything else.
+                 
                  makePlain | take 2 xs == "+1" = res 2
                            | take 1 xs == "1"  = res 1
                            | otherwise         = res 0
 
                             where res n = [x | x <- drop n xs, x >= '0', x <= '9']
                 
+
+                 --checks if the obtained 'plainStr' is of valid format: NXXNXXXXXX
+                 --where N <- [2..9], X <- [0..9] and length of the phone no. = 10.
+              
                  isValid = length plainStr == 10
                            && not (elem (plainStr !! 0) "01")
                            && not (elem (plainStr !! 3) "01")
 
 
-
-
-
-
-{- 
-  "makePlain" function:
-     takes the given string and returns another string consisting of only
-     only such chars that represent numbers, disregards everything else.
-
-
-  "isValid" predicate:
-     checks whether the obtained 'plainStr' is of valid format: NXXNXXXXXX
-     where N <- [2..9], X <- [0..9] and length of the phone no. should be 10.
-
-     Note: The case where country code is any value other than '1', gets handled
-           automatically.
-
-           [If it is given and is of any value other than '1' or '+1', 
-            the 'plainStr' comes out to be of length > 10, and so it 
-            gets rejected inside the "isValid" predicate.]
-
--}
+--Note: The case where country code is a value other than '1', gets 
+  --handled automatically by the tests laid under 'isValid' predicate.
 
 
 
